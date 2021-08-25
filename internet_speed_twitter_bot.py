1e851ce52d220c9be9d6bf1ccddd2190c30bf4fa
Kamil Ruchaj <kamilruchaj@gmail.com>
2021-08-25 18:58:10 +0200

    A few changes

--- internet_speed_twitter_bot.py
+++ internet_speed_twitter_bot.py
@@ -3,39 +3,35 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import random
-
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class InternetSpeedTwitterBot:
     def __init__(self):
-        self.down = PROMISED_DOWN
-        self.up = PROMISED_UP
+        self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
         self.message = ""
 
     def get_internet_speed(self):
-        driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
-        driver.get(SPEEDTEST_URL)
-
-        cookie = driver.find_element_by_id("_evidon-banner-acceptbutton")
+        self.driver.get(SPEEDTEST_URL)
+        cookie = self.driver.find_element_by_id("_evidon-banner-acceptbutton")
         cookie.click()
 
-        go_button = driver.find_element_by_class_name("start-text")
+        go_button = self.driver.find_element_by_class_name("start-text")
         go_button.click()
         time.sleep(60)
 
-        pop_up = driver.find_element_by_xpath(
+        pop_up = self.driver.find_element_by_xpath(
             "/html/body/div[3]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/div[2]/a")
         pop_up.click()
         time.sleep(1)
 
-        results = driver.find_elements_by_xpath("//div[@class='result-data u-align-left']")
+        results = self.driver.find_elements_by_xpath("//div[@class='result-data u-align-left']")
 
         self.message = f"Ping: {results[0].text}\nDownload: {results[1].text}\nUpload: {results[2].text}"
+        self.driver.quit()
 
-    def tweet_at_provider(self):
+    def tweet_at_provider(self, input_message):
         print("TWEET")
         driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
         driver.get(TWITTER_URL_LOGIN)
@@ -60,11 +56,15 @@ class InternetSpeedTwitterBot:
         except:
             pass
 
+        time.sleep(1)
+
         PATH = "/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div[2]/div/div/div/div"
         text = driver.find_element_by_xpath(PATH)
-        text.send_keys(self.message)
+        text.send_keys(self.message + "\n" + input_message)
+        text.send_keys(Keys.ENTER)
 
         time.sleep(1)
 
         tweet_button = driver.find_element(By.XPATH, "//div[@data-testid='tweetButtonInline']")
         tweet_button.click()
+        self.driver.quit()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from internet_speed_twitter_bot import InternetSpeedTwitterBot
+from password import MY_TWITTER_ACCOUNT
 
 bot = InternetSpeedTwitterBot()
 
 bot.get_internet_speed()
-bot.tweet_at_provider()
+bot.tweet_at_provider(input_message=MY_TWITTER_ACCOUNT)
